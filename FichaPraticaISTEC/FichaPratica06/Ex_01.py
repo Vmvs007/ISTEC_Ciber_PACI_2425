0528447de1fa5a75f6e8e5bd3934f5500fad6d19
Vitor Santos <vmvs007@gmail.com>
2024-12-09 09:51:10 +0000

    FichaPratica06

--- FichaPraticaISTEC/FichaPratica06/Ex_01.py
+++ FichaPraticaISTEC/FichaPratica06/Ex_01.py
@@ -1,11 +1,7 @@
-utilizador={}
+utilizador = {}
 
-nomeUser = input("Insira o seu nome: ")
-idadeUser = input("Insira a sua idade: ")
-cidadeUser = input("Insira a sua cidade: ")
-
-utilizador["nome"]=nomeUser
-utilizador["idade"]=idadeUser
-utilizador["cidade"]=cidadeUser
+utilizador["nome"] = input("Insira o seu nome: ")
+utilizador["idade"] = input("Insira a sua idade: ")
+utilizador["cidade"] = input("Insira a sua cidade: ")
 
 print(utilizador)
---
+++ FichaPraticaISTEC/FichaPratica06/Ex_02.py
@@ -0,0 +1,11 @@
+carro = {
+    'marca': 'Toyota',
+    'modelo': 'Corolla',
+    'ano': 2020,
+    'cor': 'Branco'
+}
+
+if ('cor' not in carro):
+    carro['cor'] = 'Azul'
+
+print(carro)
